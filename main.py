8a3e9a10240e375dd059d5e6e8c44deb0c6a3dba
ddjerqq <gio2004n@gmail.com>
2022-02-10 02:11:22 +0400

    fixed errors, added calculations made it cleaner

--- main.py
+++ main.py
@@ -1,45 +1,76 @@
-import os
-import json
-import time
 import requests
 import threading
-import sqlite3
-import ipfsApi
 
 # local
 from utils import *
 from headers import random_useragent
+from database import database as db
 
 # constants
-THREADS = 10  # change this to 4x your threads.
-data = []
-progress = 0
+_t_lock = threading.Lock()
+THREADS = 20  # change this to 4x your threads.
+progress = 1
 failed = 0
 
 
-def _t_pull(start: int, amount: int, collection: str):
+def _t_pull(start: int, amount: int, collection_url: str, collection_name: str):
     global progress, failed
     for i in range(start, start+amount):
-        uri = f"https://meta.hapeprime.com/{i}"
-
         r = requests.get(
-            uri,
-            headers={
-                "user-agent": random_useragent()
-            }
+            f"{collection_url}/{i}",
+            headers={"user-agent": random_useragent()}
         )
+
         if r.status_code != 200:
-            # rgb(str(r.status_code), "#ff0000")
-            failed += 1
-            progress += 1
-            continue
+            r2 = requests.get(
+                f"{collection_url}/{i}",
+                headers = {"user-agent": random_useragent()}
+            )
+            if r2.status_code != 200:
+                failed += 1
+                progress += 1
+                if not failed % 3:
+                    db.__save__()
+                continue
+            else:
+                r = r2
+
+        # print(r.json())
 
         progress += 1
-        # TODO CONVERT TO SQLITE
-        data.append(r.json())
+        db.add_attributes(collection_name, r.json())
 
 
-def get_collection_data(collection: str, number_of_items: int):
+"""
+
+{
+    'name': 'HAPE #89', 
+    'description': '8192 next-generation, 
+    high-fashion HAPES.', 
+    'image': 'https://meta.hapeprime.com/89.png', 
+    'external_url': 'https://hapeprime.com', 
+    'attributes': [
+        {'trait_type': 'Fur', 'value': 'Geometric Urban'}, 
+        {'trait_type': 'Head', 'value': 'Pained'}, 
+        {'trait_type': 'Tattoos', 'value': 'Gambler'}, 
+        {'trait_type': 'Teeth', 'value': 'Golden Tooth'}, 
+        {'trait_type': 'Eyes', 'value': 'Blue}, 
+        {'trait_type': 'Clothing', 'value': 'HAPE T-Shirt (Desert Storm)'}, 
+        {'trait_type': 'Accessory', 'value': 'Backpack (Velvet Violet)'}, 
+        {'trait_type': 'Jewellery', 'value': 'HAPE Cuban Link (Gold)'}, 
+        {'trait_type': 'Headwear', 'value': 'Cat Headband (Black)'}, 
+        {'trait_type': 'Eyewear', 'value': 'Female Bug Eyes (Red)'}, 
+        {'trait_type': 'Earrings', 'value': 'Diamond'}, 
+        {'trait_type': 'Birthday', 'value': '07/02'}, 
+        {'trait_type': 'Heart Number','value': '363382404504'}
+    ]
+}
+"""
+
+
+
+
+def get_collection_data(collection_url: str, number_of_items: int, collection_name: str):
     amount = round(number_of_items / THREADS)
     threads = []
     for start in range(0, THREADS+1):
@@ -47,15 +78,26 @@ def get_collection_data(collection: str, number_of_items: int):
         if start == THREADS:
             thread = threading.Thread(
                 target = _t_pull,
-                args = (start * amount, amount + round(number_of_items % THREADS), collection)
+                args = (
+                    start * amount,
+                    amount + round(number_of_items % THREADS),
+                    collection_url,
+                    collection_name
+                )
             )
         else:
             thread = threading.Thread(
                 target=_t_pull,
-                args=(start * amount, amount, collection)
+                args=(
+                    start * amount,
+                    amount,
+                    collection_url,
+                    collection_name
+                )
             )
         threads.append(thread)
         thread.start()
+    return threads
 
 
 def _t_progress_ticker(total: int):
@@ -67,6 +109,11 @@ def _t_progress_ticker(total: int):
         )
         if progress >= total:
             break
+    rgb(
+        f"\r[+] {total:<5}/ {total} | failed: {failed} | 100.00%",
+        "#00ff00",
+        newline = False
+    )
 
 
 def setup():
@@ -77,20 +124,34 @@ def setup():
 
 
 def main():
-    global data
     setup()
 
-    rgb("[?] Enter collection's name/ipfs id", "#ffff00", newline=False)
+    rgb("[?] Enter collection's url", "#ffff00", newline=False)
+    collection_url = input(" > ")
+    if collection_url.endswith("/"):
+        collection_url = collection_url[:-1]
+
+    if not collection_url:
+        rgb("[!] No collection url entered! quitting!", "#ff0000")
+        exit()
+
+    rgb("[?] Enter collection name", "#ffff00", newline=False)
     collection_name = input(" > ")
+    if collection_name:
+        db.create_table(collection_name)
+    else:
+        rgb("[!] No collection name entered! quitting!", "#ff0000")
+        exit()
+
     rgb("[?] Enter number of items you want to pull", "#ffff00", newline=False)
     number_of_items = int(input(" > "))
+
     time_start = time.time()
-    rgb("[+] Started pulling data", "#00ff00")
+    rgb(f"[+] Started pulling data from {collection_name}", "#00ff00")
     rgb(f"[+] Threads: {THREADS} | Amount per Thread {round(number_of_items / THREADS)}", "#00ff00")
 
     try:
-        # we need "try" to save the data
-        get_collection_data(collection_name, number_of_items)
+        get_collection_data(collection_url, number_of_items, collection_name)
         counter = threading.Thread(target=_t_progress_ticker, args=(number_of_items,))
         counter.start()
         counter.join()
@@ -102,18 +163,17 @@ def main():
         rgb(f"\n[!] {e}", "#ff0000")
 
     finally:
-        rgb(f"\n[+] {len(data) - failed} items pulled in {round(time.time() - time_start, 4)} seconds", "#00ff00")
+        rgb(f"\n[+] {db.size_of_table(collection_name)} "
+            f"items pulled successfully in {round(time.time() - time_start, 2)} seconds", "#00ff00")
         if failed > 0:
-            rgb(f"[-] {failed} items failed", "#ff0000")
+            rgb(f"[-] {failed} items failed {round(failed / number_of_items * 100, 2)}% of total", "#ff0000")
 
-        data = sorted(data, key=lambda x: int(x["name"].split("#")[1]))
-
-        with open(f"data\\{collection_name}.json", "w") as f:
-            json.dump(
-                data,
-                f,
-                indent=4,
-            )
+        rarest_attributes = db.rarest_attributes(collection_name)
+        for rarest_attributes in rarest_attributes:
+            for attr in db.rarest_values_of_attribute(collection_name, rarest_attributes[0]):
+                total = db.total_number_of_values(collection_name)
+                rgb(f"{attr[0]} has {attr[1]} {attr[2]}/{total}", "#00ff00")
+                rgb(f"{round((attr[2] / total) * 100, 4)}%", "#00ff00")
 
 
 if __name__ == "__main__":
