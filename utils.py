f829acf8e887f7083dfa0779c3583e920fd1327b
Giorgi Nachkebia <gio2004n@gmail.com>
2022-02-16 13:54:01 +0400

    empty commit

--- utils.py
+++ utils.py
@@ -190,7 +190,6 @@ def pretty_iterable(iterable: Iterable) -> None:
     rgb(f"╚═══════╩═{'═' * max_size}═╝", color = g)
 
 
-
 if __name__ == "__main__":
     welcome_screen()
     pretty_iterable([str(i) * i for i in range(1, 50)])
