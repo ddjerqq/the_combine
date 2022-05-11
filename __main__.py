b3c96a74f3afc0ade78773def6528b192b7bfd82
Giorgi Nachkebia <gio2004n@gmail.com>
2022-05-12 01:22:11 +0400

    V3 started develop

--- __main__.py
+++ __main__.py
@@ -1,34 +1,24 @@
-from utils import *
-from database import database as db
-from demons import spawn_demons
+# STEP 1: get the IPFS hash from user
+#     and get the file from IPFS
+# STEP 2: determine the amount of items in the collection
+#     and get the collection from IPFS
+# STEP 3: start downloading the metadata
+# STEP 4: start analyzing the metadata
+# STEP 5: get the rarest nfts
 
 
-def main() -> None:
-    os.system(f"title \"NFT SNYPER | DDJERQQ\"")
+LOGO = """
+███████╗ ███╗   ██╗ ██╗   ██╗ ██████╗  ███████╗ ██████╗ 
+██╔════╝ ████╗  ██║ ╚██╗ ██╔╝ ██╔══██╗ ██╔════╝ ██╔══██╗
+███████╗ ██╔██╗ ██║  ╚████╔╝  ██████╔╝ █████╗   ██████╔╝
+╚════██║ ██║╚██╗██║   ╚██╔╝   ██╔═══╝  ██╔══╝   ██╔══██╗
+███████║ ██║ ╚████║    ██║    ██║      ███████╗ ██║  ██║
+╚══════╝ ╚═╝  ╚═══╝    ╚═╝    ╚═╝      ╚══════╝ ╚═╝  ╚═╝"""
 
-    c_name, c_uri, j_end = prompt()
-
-    if not db.table_exists(c_name):
-        db.create_table_if_not_exists(c_name)
-        os.system(f"title \"NFT SNYPER | DDJERQQ | {c_name}\"")
-
-        _time_start = time.time()
-        rgb(f"[#] Please wait...", color=0xffff00, newline=False)
-        number_of_items = binary_find(c_uri, j_end)
-        clear()
-        spawn_demons(c_uri, c_name, number_of_items, j_end)
-        db.__save__(c_name)
-
-    items = db.get_rarest_items(c_name, 20)
-
-    if items:
-        pretty_iterable([x[1] for x in items])
+# PROXY = {"http": "http://metacircuits:dZwUllzyyZWL41U0@p.litespeed.cc:31112"}
+# HEADER_GEN = lambda: {"accept": "application/json", "user-agent": random_useragent()}
+# ABSOLUTE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-if __name__ == "__main__":
-    try:
-        clear()
-        main()
-    except KeyboardInterrupt:
-        _exit = vinput("\n[?] Enter to exit", lambda x: True)
-        sys.exit()
+async def main():
+    ...
